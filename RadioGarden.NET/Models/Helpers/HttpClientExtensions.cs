5f4d49cbb37af7c5b8e0d97be611632c10982d09
Julio Gabriel <juliogabriel516@gmail.com>
2024-08-29 15:07:36 -0300

    Full port to HttpClient

---
+++ RadioGarden.NET/Models/Helpers/HttpClientExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RadioGarden.NET.Models.Converters;
+
+namespace RadioGarden.NET.Models.Helpers
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="HttpClient"/> class.
+    /// </summary>
+    internal static class HttpClientExtensions
+    {
+        /// <summary>
+        /// A static instance of <see cref="JsonSerializerOptions"/> configured with custom converters.
+        /// </summary>
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+        {
+            Converters = { new GeoConverter() }
+        };
+
+        /// <summary>
+        /// Sends an HTTP GET request and deserializes the JSON response content to the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to which the JSON response content is deserialized.</typeparam>
+        /// <param name="httpClient">The <see cref="HttpClient"/> instance used to send the request.</param>
+        /// <param name="requestMessage">The <see cref="HttpRequestMessage"/> representing the HTTP GET request.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The task result contains the deserialized object of type <typeparamref name="T"/> if the request is successful; otherwise, <c>null</c>.
+        /// </returns>
+        internal static async Task<T?> SendGetJsonAsync<T>(this HttpClient httpClient,
+            HttpRequestMessage requestMessage)
+            where T : class
+        {
+            var response = await httpClient.SendAsync(requestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<T>(json, SerializerOptions);
+            }
+
+            return null;
+        }
+    }
+}
--- RadioGarden.NET/RadioGardenClient.cs
+++ RadioGarden.NET/RadioGardenClient.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Text.Json;
+using System.Net.Http;
 using System.Threading.Tasks;
 using RadioGarden.NET.Models;
-using RadioGarden.NET.Models.Converters;
-using RestSharp;
-using RestSharp.Serializers.Json;
+using RadioGarden.NET.Models.Helpers;
 
 namespace RadioGarden.NET
 {
@@ -13,29 +11,17 @@ namespace RadioGarden.NET
     /// </summary>
     public class RadioGardenClient
     {
-        private readonly RestClient _restClient;
+        private readonly HttpClient _httpClient;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RadioGardenClient"/> class.
         /// </summary>
         public RadioGardenClient()
         {
-            // ConfigureMessageHandler = handler => new HttpClientHandler(),
-
-            var restOptions = new RestClientOptions
+            _httpClient = new HttpClient()
             {
-                BaseUrl = new Uri("https://radio.garden/api"),
+                BaseAddress = new Uri("https://radio.garden/api"),
             };
-
-            var serializerOptions = new JsonSerializerOptions()
-            {
-                Converters = { new GeoConverter() }
-            };
-
-
-            _restClient = new RestClient(restOptions,
-                configureSerialization: s => s.UseSystemTextJson(serializerOptions)
-            );
         }
 
         #region Places
@@ -48,25 +34,14 @@ namespace RadioGarden.NET
         /// </returns>
         public async Task<PlacesResponse?> GetAllPlacesAsync()
         {
-            var request = new RestRequest("ara/content/places");
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "/api/ara/content/places");
 
-            var response = await _restClient.GetAsync<PlacesResponse>(request);
+            var response = await _httpClient.SendGetJsonAsync<PlacesResponse>(request);
+
             return response;
         }
 
-        /// <summary>
-        /// Retrieves places with registered radio stations.
-        /// </summary>
-        /// <returns>
-        /// An <see cref="PlacesResponse"/> object containing the list of places with radio stations, or null if the request fails.
-        /// </returns>
-        public PlacesResponse? GetAllPlaces()
-        {
-            var request = new RestRequest("ara/content/places");
-
-            var response = _restClient.Get<PlacesResponse>(request);
-            return response;
-        }
 
         /// <summary>
         /// Retrieves the list of registered radio stations for a specified place asynchronously.
@@ -81,26 +56,9 @@ namespace RadioGarden.NET
             if (string.IsNullOrEmpty(placeId))
                 throw new ArgumentException("Parameter Id can't be null or empty.");
 
-            var request = new RestRequest($"ara/content/page/{placeId}/channels");
-            var response = await _restClient.GetAsync<PlaceChannelsResponse>(request);
-            return response;
-        }
-
-        /// <summary>
-        /// Retrieves the list of registered radio stations for a specified place.
-        /// </summary>
-        /// <param name="placeId">The unique identifier of the place.</param>
-        /// <returns>
-        /// A <see cref="PlaceChannelsResponse"/> object containing the list of radio channels for the specified place, or null if the request fails.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="placeId"/> is null or empty.</exception>
-        public PlaceChannelsResponse? GetPlaceChannels(string placeId)
-        {
-            if (string.IsNullOrEmpty(placeId))
-                throw new ArgumentNullException(nameof(placeId), "Parameter Id can't be null or empty.");
-
-            var request = new RestRequest($"ara/content/page/{placeId}/channels");
-            var response = _restClient.Get<PlaceChannelsResponse>(request);
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                $"api/ara/content/page/{placeId}/channels");
+            var response = await _httpClient.SendGetJsonAsync<PlaceChannelsResponse>(request);
             return response;
         }
 
@@ -121,28 +79,13 @@ namespace RadioGarden.NET
         {
             if (string.IsNullOrEmpty(channelId))
                 throw new ArgumentNullException(nameof(channelId), "channelId can't be null or empty.");
-            var request = new RestRequest($"ara/content/channel/{channelId}");
-            var response = await _restClient.GetAsync<ChannelDetailsResponse?>(request);
+
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                $"api/ara/content/channel/{channelId}");
+            var response = await _httpClient.SendGetJsonAsync<ChannelDetailsResponse>(request);
             return response;
         }
 
-        /// <summary>
-        /// Retrieves detailed information about a specific radio channel.
-        /// </summary>
-        /// <param name="channelId">The ID of the radio channel to retrieve details for.</param>
-        /// <returns>
-        /// A <see cref="Task{ChannelDetailsResponse}"/> representing the asynchronous operation, 
-        /// with a <see cref="ChannelDetailsResponse"/> containing the channel details, or null if the request fails.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="channelId"/> is null or empty.</exception>
-        public ChannelDetailsResponse? GetChannelDetails(string channelId)
-        {
-            if (string.IsNullOrEmpty(channelId))
-                throw new ArgumentNullException(nameof(channelId), "channelId can't be null or empty.");
-            var request = new RestRequest($"ara/content/channel/{channelId}");
-            var response = _restClient.Get<ChannelDetailsResponse?>(request);
-            return response;
-        }
 
         /// <summary>
         /// Retrieves the broadcast stream URL for a specific radio channel asynchronously.
@@ -158,35 +101,14 @@ namespace RadioGarden.NET
             if (string.IsNullOrEmpty(channelId))
                 throw new ArgumentNullException(nameof(channelId), "channelId can't be null or empty.");
 
-            var request = new RestRequest($"ara/content/listen/{channelId}/channel.mp3");
-            var response = await _restClient.HeadAsync(request);
+            var request = new HttpRequestMessage(HttpMethod.Head,
+                $"api/ara/content/listen/{channelId}/channel.mp3");
 
-            return response.IsSuccessful
-                   && response.ResponseUri != null
-                ? response.ResponseUri.ToString()
-                : null;
-        }
+            var response = await _httpClient.SendAsync(request);
 
-        /// <summary>
-        /// Retrieves the broadcast stream URL for a specific radio channel.
-        /// </summary>
-        /// <param name="channelId">The ID of the radio channel.</param>
-        /// <returns>
-        /// A <see cref="Task{String}"/> representing the asynchronous operation, 
-        /// with a string containing the broadcast stream URL if successful, or an empty string if the request fails.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="channelId"/> is null or empty.</exception>
-        public string? GetChannelStreamUrl(string channelId)
-        {
-            if (string.IsNullOrEmpty(channelId))
-                throw new ArgumentNullException(nameof(channelId), "channelId can't be null or empty.");
-
-            var request = new RestRequest($"ara/content/listen/{channelId}/channel.mp3");
-            var response = _restClient.Head(request);
-
-            return response.IsSuccessful
-                   && response.ResponseUri != null
-                ? response.ResponseUri.ToString()
+            return response.IsSuccessStatusCode &&
+                   response.RequestMessage?.RequestUri != null
+                ? response.RequestMessage.RequestUri.ToString()
                 : null;
         }
 
@@ -208,35 +130,16 @@ namespace RadioGarden.NET
             if (string.IsNullOrEmpty(query))
                 throw new ArgumentNullException(nameof(query), "Query can't be null or empty.");
 
-            var request = new RestRequest($"search?q={query}");
-            var response = await _restClient.GetAsync<QueryResponse>(request);
-            return response;
-        }
-
-
-        /// <summary>
-        /// Search countries, places, and radio stations.
-        /// </summary>
-        /// <param name="query">The search query string.</param>
-        /// <returns>
-        /// A <see cref="Task{QueryResponse}"/> representing the asynchronous operation, 
-        /// with a <see cref="QueryResponse"/> containing the search results, or null if the request fails.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="query"/> is null or empty.</exception>
-        public QueryResponse? Search(string query)
-        {
-            if (string.IsNullOrEmpty(query))
-                throw new ArgumentNullException(nameof(query), "Query can't be null or empty.");
-
-            var request = new RestRequest($"search?q={query}");
-            var response = _restClient.Get<QueryResponse>(request);
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                $"api/search?q={query}");
+            var response = await _httpClient.SendGetJsonAsync<QueryResponse>(request);
             return response;
         }
 
         #endregion
 
         #region Geo
-        
+
         /// <summary>
         /// Retrieves the client's geolocation asynchronously.
         /// </summary>
@@ -246,26 +149,12 @@ namespace RadioGarden.NET
         /// </returns>
         public async Task<GeoLocation?> GetClientGeoLocationAsync()
         {
-            var request = new RestRequest("geo");
-            var response = await _restClient.GetAsync<GeoLocation>(request);
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "api/geo");
+            var response = await _httpClient.SendGetJsonAsync<GeoLocation>(request);
             return response;
         }
-        
-        /// <summary>
-        /// Retrieves the client's geolocation.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="Task{GeoLocation}"/> representing the asynchronous operation, 
-        /// with an <see cref="GeoLocation"/> object containing the client's geolocation data, or null if the request fails.
-        /// </returns>
-        public GeoLocation? GetClientGeoLocation()
-        {
-            var request = new RestRequest("geo");
-            var response = _restClient.Get<GeoLocation>(request);
-            return response;
-        }
-        
-        
+
         #endregion
     }
 }
